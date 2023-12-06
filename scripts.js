3e72f1bb248ca609239e51827f3908f79b8dcd3a
Hussain Almakana <53603723+7aso0ni@users.noreply.github.com>
2023-12-06 06:04:57 +0000

    .

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
-const registerForm = document.getElementById("register-form");
+document.addEventListener("DOMContentLoaded", function () {
+  const registerForm = document.getElementById("register-form");
 
-registerForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the default form submission
+  registerForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-  // Get form field values
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    // Get form field values
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  // Store each form field value individually in local storage
-  localStorage.setItem("firstName", firstName);
-  localStorage.setItem("lastName", lastName);
-  localStorage.setItem("username", username);
-  localStorage.setItem("email", email);
-  localStorage.setItem("password", password);
+    // Check if any field is empty
+    if (!firstName || !lastName || !username || !email || !password) {
+      return;
+    }
+
+    // Store each form field value individually in local storage
+    localStorage.setItem("firstName", firstName);
+    localStorage.setItem("lastName", lastName);
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+  });
 });
